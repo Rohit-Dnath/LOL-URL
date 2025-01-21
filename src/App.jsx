d5d7b362abfe5b26a36a84d3382f109916d37030
rohit-dnath <debnathrohit97@gmail.com>
2025-01-22 04:13:57 +0530

    Add RequireAuth component to protect routes and enhance authentication flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,58 @@
-import { createBrowserRouter, RouterProvider, } from 'react-router-dom';
-import './App.css'
-import React from 'react'
-import './index.css'
-import AppLayout from './layouts/app-layout';
-import Dashboard from './pages/dashboard';
-import LandingPage from './pages/landing';
-import Auth from './pages/auth';
-import Link from './pages/link';
-import RedirectLink from './pages/redirect-link';
-import UrlProvider from './context';
-
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import React from "react";
+import "./index.css";
+import AppLayout from "./layouts/app-layout";
+import Dashboard from "./pages/dashboard";
+import LandingPage from "./pages/landing";
+import Auth from "./pages/auth";
+import Link from "./pages/link";
+import RedirectLink from "./pages/redirect-link";
+import UrlProvider from "./context";
+import RequireAuth from "./components/require-auth";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
     children: [
       {
-        path : '/',
-      element: <LandingPage />
+        path: "/",
+        element: <LandingPage />,
       },
       {
-        path : '/dashboard',
-      element: <Dashboard />
+        path: "/dashboard",
+        element: (
+          <RequireAuth>
+            <Dashboard />
+          </RequireAuth>
+        ),
       },
       {
-        path : '/auth',
-      element: <Auth />
+        path: "/auth",
+        element: <Auth />
       },
       {
-        path : '/link/:id',
-      element: <Link />
+        path: "/link/:id",
+        element: (
+          <RequireAuth>
+            <Link />
+          </RequireAuth>
+        ),
       },
       {
-        path : '/:id',
-      element: <RedirectLink />
+        path: "/:id",
+        element: <RedirectLink />,
       },
-
-
-    ]
-  }
-])
+    ],
+  },
+]);
 
 function App() {
-  
-
   return (
     <UrlProvider>
       <RouterProvider router={router} />
     </UrlProvider>
-  )
-
-  
+  );
 }
 
-export default App
+export default App;
