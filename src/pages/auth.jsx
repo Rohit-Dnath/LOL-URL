cc8aecda735fd5a2a65020a9bc4882a68f81f319
rohit-dnath <debnathrohit97@gmail.com>
2025-01-22 04:00:49 +0530

    Redirect authenticated users to the dashboard and improve login message display

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,16 +1,31 @@
-import React from "react";
-import { useSearchParams } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Login from "@/components/login";
 import Signup from "@/components/signup";
+import { UrlState } from "@/context";
 
 
 const Auth = () => {
   const [SearchParams] = useSearchParams();
+  const longLink = SearchParams.get("createNew");
+  const navigate = useNavigate();
+
+  const { isAuthenticated, loading } = UrlState();
+  
+
+  useEffect(() => {
+    
+    if (isAuthenticated) {
+      navigate(`/dashboard?${longLink ? `createNew=${longLink}` : ""}`);
+    }
+  }, [isAuthenticated, loading])
+  
+
   return (
     <div className="mt-24 flex flex-col items-center gap-10">
       <h1 className="text-5xl font-extrabold">
-        {SearchParams.get("createNew")
+        {longLink
           ? "Hold up! Lets's Login first"
           : "Login / Signup"}
       </h1>
