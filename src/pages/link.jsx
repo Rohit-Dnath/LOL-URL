f69f7b616540a9be429747c4d35c3d6902242b5c
rohit-dnath <debnathrohit97@gmail.com>
2025-02-01 03:58:43 +0530

    Add ScrollProgress component to LinkPage for enhanced user experience

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -141,6 +141,7 @@ const LinkPage = () => {
 
   return (
     <>
+    <ScrollProgress />
       {isNewLink && <AutoConfetti />}
       <ToastContainer position="bottom-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover style={{ zIndex: 9999 }} />
       {(loading || loadingStats) && (
